fcc60fcb16aaad9ad2199af5dd1af264ad20ee6e
sudiptadutta3107 <User@DESKTOP-S12QBVO>
2025-03-06 17:07:09 +0530

    Implemented do-while loop

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,33 +7,41 @@ namespace ConsoleApp1
     {
         static void Main()
         {
-            Console.Write("\n Enter first number: ");
-            int FirstNumber = int.Parse(Console.ReadLine());
+            String Operator = string.Empty;
 
-            Console.Write("\n Enter second number: ");
-            int SecondNumber = int.Parse(Console.ReadLine());
-
-            Console.Write("\n Enter the operator [+ - * /]: ");
-            String Operator = Console.ReadLine();
-
-            switch (Operator)
+            do
             {
-                case "+":
-                    Console.Write("Addition = {0} ", Calculator.Addition(FirstNumber, SecondNumber));
-                    break;
-                case "-":
-                    Console.Write("Substraction = {0} ", Calculator.Substraction(FirstNumber, SecondNumber));
-                    break;
-                case "*":
-                    Console.Write("Multiplication = {0} ", Calculator.Multiplication(FirstNumber, SecondNumber));
-                    break;
-                case "/":
-                    Console.Write("Division = {0} ", Calculator.Division(FirstNumber, SecondNumber));
-                    break;
-                default :
-                    Console.Write("Invalid operator");
-                    break;
-            }
+                Console.Write("\n Enter first number: ");
+                int FirstNumber = int.Parse(Console.ReadLine());
+
+                Console.Write("\n Enter second number: ");
+                int SecondNumber = int.Parse(Console.ReadLine());
+
+                Console.Write("\n Enter the operator [+ - * /]: ");
+                Operator = Console.ReadLine();
+
+                switch (Operator)
+                {
+                    case "+":
+                        Console.Write("Addition = {0} ", Calculator.Addition(FirstNumber, SecondNumber));
+                        break;
+                    case "-":
+                        Console.Write("Substraction = {0} ", Calculator.Substraction(FirstNumber, SecondNumber));
+                        break;
+                    case "*":
+                        Console.Write("Multiplication = {0} ", Calculator.Multiplication(FirstNumber, SecondNumber));
+                        break;
+                    case "/":
+                        Console.Write("Division = {0} ", Calculator.Division(FirstNumber, SecondNumber));
+                        break;
+                    default:
+                        Console.Write("Invalid operator");
+                        break;
+                }
+            } while (Operator != string.Empty);
+
+
+            
         }
     }
 }
